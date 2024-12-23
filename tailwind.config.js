ce64abc57e0b5e275d4918e6b84cb7de8942f823
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-12-23 14:31:27 +0200

    feat: created initial App dependecies

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // <-- Эти пути должны покрывать все ваши файлы
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
